2fb844c0676377a518954be8446e8c609509824f
stealthy-warrior <31582478+stealthy-warrior@users.noreply.github.com>
2017-09-07 22:17:34 +0500

    Add files via upload

---
+++ задание_3.js
@@ -0,0 +1,24 @@
+﻿var a=1;
+var b=2;
+var c=3;
+var d=4;
+var e=5;
+var f=6;
+var g=7;
+var h=8;
+var i=9;
+var j=10;
+var num=1;
+while (num <= 10) {
+document.write(num * a + ' ');
+document.write(num * b + ' ');
+document.write(num * c + ' ');
+document.write(num * d + ' ');
+document.write(num * e + ' ');
+document.write(num * f + ' ');
+document.write(num * g + ' ');
+document.write(num * h + ' ');
+document.write(num * i + ' ');
+document.write(num + j + ' ' + '<br>');
+num +=1;
+}
