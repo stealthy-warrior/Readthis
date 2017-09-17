c6c5fec576c401b36c7671e147db395a9f278d93
stealthy-warrior <31582478+stealthy-warrior@users.noreply.github.com>
2017-09-17 22:22:52 +0500

    Update задание_1.js

--- задание_1.js
+++ задание_1.js
@@ -1,5 +1,5 @@
 var str1 = prompt('Введите любое число от 100 до 999');
-str1 = str1[0] + (Math.floor(Math.random() * 10)) + str1[2];
 var str2 = prompt('Введите любое число от 100 до 999');
-str2 = str2[0] + (Math.floor(Math.random() * 10)) + str2[2];
-alert( str1 + ' ' + str2);
+str3 = str1[0] + str2[1] + str1[2];
+str4 = str2[0] + str1[1] + str2[2];
+alert( str3 + ' ' + str4);
