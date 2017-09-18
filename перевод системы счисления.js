7d51febdc563b2a78093cbdd7b450339b785382c
stealthy-warrior <31582478+stealthy-warrior@users.noreply.github.com>
2017-09-18 22:27:08 +0500

    Add files via upload

---
+++ перевод системы счисления.js
@@ -0,0 +1,6 @@
+﻿var num = parseInt(prompt('Введите число'));
+var notation = parseInt(prompt('Выберите систему счисления (введите 2, 8 или 16'));
+if (notation == 2) {alert( num.toString(2));}
+else if (notation ==8) {alert( num.toString(8));}
+else if (notation ==16) {alert( num.toString(16));}
+else {alert('Неверный ввод');}
